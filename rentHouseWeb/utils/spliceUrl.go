d6232db1167c09ae7fcd5e5d69aa522154b860d9
Prudenter <18706733725@163.com>
2019-06-14 19:37:32 +0800

    区域服务添加redis功能

--- rentHouseWeb/utils/spliceUrl.go
+++ rentHouseWeb/utils/spliceUrl.go
@@ -4,6 +4,16 @@
 */
 
 package utils
+
+import (
+	"github.com/astaxie/beego/cache"
+	"encoding/json"
+	"fmt"
+	_ "github.com/gomodule/redigo/redis"
+	_ "github.com/garyburd/redigo/redis"
+	_ "github.com/astaxie/beego/cache/redis"
+)
+
 /*
 	url请求拼接函数
 	将http://IP:PORT + url进行拼接
@@ -11,4 +21,24 @@ package utils
 func SpliceUrl(url string) (reqUrl string) {
 	reqUrl = "http://" + G_fastdfs_addr + ":" + G_fastdfs_port + "/" + url
 	return
+}
+
+/*
+	redis连接函数
+*/
+func RedisOpen(server_name, redis_addr, redis_port, redis_dbnum string) (bm cache.Cache, err error) {
+	redis_config_map := map[string]string{
+		"key":   server_name,
+		"conn":  redis_addr + ":" + redis_port,
+		"dbNum": redis_dbnum,
+	}
+	// 将配置信息的map转化为json
+	redis_config, _ := json.Marshal(redis_config_map)
+	// 连接redis数据库
+	bm, err = cache.NewCache("redis", string(redis_config))
+	if err != nil {
+		fmt.Println("连接redis错误", err)
+		return nil, err
+	}
+	return bm, nil
 }
