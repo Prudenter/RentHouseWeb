3337f1882a4227813aefa4d66b68b52158dbb049
Prudenter <18706733725@163.com>
2019-06-14 19:37:17 +0800

    区域服务添加redis功能

--- getArea/handler/example.go
+++ getArea/handler/example.go
@@ -7,6 +7,8 @@ import (
 	"RentHouseWeb/rentHouseWeb/utils"
 	"github.com/astaxie/beego/orm"
 	"RentHouseWeb/rentHouseWeb/models"
+	"encoding/json"
+	"time"
 )
 
 type Example struct{}
@@ -17,7 +19,41 @@ func (e *Example) GetArea(ctx context.Context, req *example.Request, rsp *exampl
 	// 1.初始化返回值
 	rsp.Errno = utils.RECODE_OK
 	rsp.Errmsg = utils.RecodeText(rsp.Errno)
-	// 2.查询数据库
+
+	// 2.调用工具类函数,获取存入redis中的数据
+	bm, err := utils.RedisOpen(utils.G_server_name, utils.G_redis_addr, utils.G_redis_port, utils.G_redis_dbnum)
+	if err != nil {
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
+	}
+	// 定义区域信息存入redis的key值
+	key := "area_info"
+	// 先从redis中获取数据
+	area_info_value := bm.Get(key)
+	// 校验数据
+	if area_info_value != nil {
+		fmt.Println("已从redis中获取到数据,准备发送给web端..")
+		// 接收数据
+		var areas []models.Area
+		// 解码
+		err := json.Unmarshal(area_info_value.([]byte), &areas)
+		if err != nil {
+			fmt.Println("redis数据解码错误", err)
+			rsp.Errno = utils.RECODE_DBERR
+			rsp.Errmsg = utils.RecodeText(rsp.Errno)
+			return nil
+		}
+		// 循环转换数据发送给web
+		for _, value := range areas {
+			area := example.ResponseAddress{AId: int32(value.Id), AName: string(value.Name)}
+			// 数据返回给web端
+			rsp.Data = append(rsp.Data, &area)
+		}
+		return nil
+	}
+
+	// 3.如果redis中获取不到数据,则需要查询数据库
 	o := orm.NewOrm()
 	// 定义接收数据的容器
 	var areas []models.Area
@@ -26,22 +62,34 @@ func (e *Example) GetArea(ctx context.Context, req *example.Request, rsp *exampl
 	//查询全部
 	n, err := qs.All(&areas)
 	if err != nil {
-		fmt.Println("查询数据库错误", err)
+		fmt.Println("查询数据库错误!", err)
 		rsp.Errno = utils.RECODE_DBERR
 		rsp.Errmsg = utils.RecodeText(rsp.Errno)
 		return nil
 	}
 	if n == 0 {
-		fmt.Println("无数据", err)
+		fmt.Println("无数据!", err)
 		rsp.Errno = utils.RECODE_DBERR
 		rsp.Errmsg = utils.RecodeText(rsp.Errno)
 		return nil
 	}
 
-	// 3.将查询到的数据转化类型
+	// 4.将查询到的数据存入redis中,方便后续使用
+	// 对数据进行json编码
+	area_info_json, _ := json.Marshal(areas)
+	//存入数据
+	bm.Put(key, area_info_json, time.Second*7200)
+	if err != nil {
+		fmt.Println("存入redis数据失败!", err)
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
+	}
+
+	// 5.将查询到的数据转化类型
 	for _, value := range areas {
 		area := example.ResponseAddress{AId: int32(value.Id), AName: string(value.Name)}
-		// 4.数据返回给web端
+		// 6.数据返回给web端
 		rsp.Data = append(rsp.Data, &area)
 	}
 	return nil
