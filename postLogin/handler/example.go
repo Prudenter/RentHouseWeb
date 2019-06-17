f3fe71079acfc4a1dcedaaa3732b774476bc5d07
Prudenter <18706733725@163.com>
2019-06-17 20:34:13 +0800

    用户登录服务srv端实现

--- postLogin/handler/example.go
+++ postLogin/handler/example.go
@@ -3,46 +3,65 @@ package handler
 import (
 	"context"
 
-	"github.com/micro/go-log"
-
 	example "RentHouseWeb/postLogin/proto/example"
+	"fmt"
+	"RentHouseWeb/rentHouseWeb/utils"
+	"github.com/astaxie/beego/orm"
+	"RentHouseWeb/rentHouseWeb/models"
+	"strconv"
+	"time"
 )
 
 type Example struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
-func (e *Example) Call(ctx context.Context, req *example.Request, rsp *example.Response) error {
-	log.Log("Received Example.Call request")
-	rsp.Msg = "Hello " + req.Name
-	return nil
-}
+func (e *Example) PostLogin(ctx context.Context, req *example.Request, rsp *example.Response) error {
+	fmt.Println("用户登录服务,PostLogin..")
 
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *Example) Stream(ctx context.Context, req *example.StreamingRequest, stream example.Example_StreamStream) error {
-	log.Logf("Received Example.Stream request with count: %d", req.Count)
+	// 1.初始化返回值
+	rsp.Errno = utils.RECODE_OK
+	rsp.Errmsg = utils.RecodeText(rsp.Errno)
 
-	for i := 0; i < int(req.Count); i++ {
-		log.Logf("Responding: %d", i)
-		if err := stream.Send(&example.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
+	// 2.查询数据
+	var user models.User
+	o := orm.NewOrm()
+	qs := o.QueryTable("User")
+	err := qs.Filter("mobile",req.Mobile).One(&user)
+	if err != nil {
+		fmt.Println("用户名查询失败",err)
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
 	}
 
-	return nil
-}
-
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *Example) PingPong(ctx context.Context, stream example.Example_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		log.Logf("Got ping %v", req.Stroke)
-		if err := stream.Send(&example.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
+	// 3.判断密码是否正确
+	if utils.Getmd5String(req.Password)!= user.Password_hash {
+		fmt.Println("密码错误",utils.Getmd5String(req.Password), user.Password_hash)
+		rsp.Errno = utils.RECODE_PWDERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
 	}
+
+	// 4.生成sessionId
+	sessionId := utils.Getmd5String(req.Mobile + req.Password + strconv.Itoa(int(time.Now().UnixNano())))
+
+	// 5.连接redis
+	bm,err := utils.RedisOpen(utils.G_server_name,utils.G_redis_addr,
+		utils.G_redis_port,utils.G_redis_dbnum)
+	if err !=nil{
+		fmt.Println("连接redis失败",err)
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
+	}
+
+	// 6.拼接key,将数据存入redis
+	bm.Put(sessionId + "user_id",user.Id,time.Second * 600)
+	bm.Put(sessionId + "user_mobile",user.Mobile,time.Second * 600)
+	bm.Put(sessionId + "user_name",user.Name,time.Second * 600)
+
+	// 7.返回sessionId
+	rsp.SessionId = sessionId
+
+	return nil
 }
--- postLogin/main.go
+++ postLogin/main.go
@@ -4,14 +4,13 @@ import (
 	"github.com/micro/go-log"
 	"github.com/micro/go-micro"
 	"RentHouseWeb/postLogin/handler"
-	"RentHouseWeb/postLogin/subscriber"
-
 	example "RentHouseWeb/postLogin/proto/example"
+	"github.com/micro/go-grpc"
 )
 
 func main() {
 	// New Service
-	service := micro.NewService(
+	service := grpc.NewService(
 		micro.Name("go.micro.srv.postLogin"),
 		micro.Version("latest"),
 	)
@@ -21,12 +20,6 @@ func main() {
 
 	// Register Handler
 	example.RegisterExampleHandler(service.Server(), new(handler.Example))
-
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("go.micro.srv.postLogin", service.Server(), new(subscriber.Example))
-
-	// Register Function as Subscriber
-	micro.RegisterSubscriber("go.micro.srv.postLogin", service.Server(), subscriber.Handler)
 
 	// Run service
 	if err := service.Run(); err != nil {
