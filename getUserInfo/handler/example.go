315cb2970e811640bd0fac05c74c1616908f2604
Prudenter <18706733725@163.com>
2019-06-18 20:35:38 +0800

    获取用户信息服务srv端实现

--- getUserInfo/handler/example.go
+++ getUserInfo/handler/example.go
@@ -2,47 +2,62 @@ package handler
 
 import (
 	"context"
-
-	"github.com/micro/go-log"
-
 	example "RentHouseWeb/getUserInfo/proto/example"
+	"fmt"
+	"RentHouseWeb/rentHouseWeb/utils"
+	"github.com/garyburd/redigo/redis"
+	"github.com/astaxie/beego/orm"
+	"RentHouseWeb/rentHouseWeb/models"
+	"strconv"
 )
 
 type Example struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
-func (e *Example) Call(ctx context.Context, req *example.Request, rsp *example.Response) error {
-	log.Log("Received Example.Call request")
-	rsp.Msg = "Hello " + req.Name
-	return nil
-}
+func (e *Example) GetUserInfo(ctx context.Context, req *example.Request, rsp *example.Response) error {
+	fmt.Println("获取用户信息服务,GetUserInfo..")
 
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *Example) Stream(ctx context.Context, req *example.StreamingRequest, stream example.Example_StreamStream) error {
-	log.Logf("Received Example.Stream request with count: %d", req.Count)
+	// 1.初始化返回值
+	rsp.Errno = utils.RECODE_OK
+	rsp.Errmsg = utils.RecodeText(rsp.Errno )
 
-	for i := 0; i < int(req.Count); i++ {
-		log.Logf("Responding: %d", i)
-		if err := stream.Send(&example.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
+	// 2.获取sessionId
+	sessionId := req.SessionId
+
+	// 3.连接redis
+	bm ,err :=utils.RedisOpen(utils.G_server_name ,utils.G_redis_addr ,utils.G_redis_port ,utils.G_redis_dbnum)
+	if err!=nil{
+		fmt.Println("redis 连接失败",err)
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno )
+		return nil
 	}
 
-	return nil
-}
+	// 4.拼接key,查询user_id
+	value := bm.Get(sessionId + "user_id")
+	value_int,_ := redis.Int(value,nil)
 
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *Example) PingPong(ctx context.Context, stream example.Example_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		log.Logf("Got ping %v", req.Stroke)
-		if err := stream.Send(&example.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
+	// 5.连接数据库
+	o := orm.NewOrm()
+	user := models.User{Id:value_int}
+
+	// 6.查询数据库
+	err = o.Read(&user)
+	err =o.Read(&user)
+	if err !=nil{
+		fmt.Println("用户数据查询失败",err)
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno )
+		return nil
 	}
+
+	// 7.返回数据
+	rsp.UserId = strconv.Itoa(user.Id)
+	rsp.Name = user.Name
+	rsp.Mobile = user.Mobile
+	rsp.RealName = user.Real_name
+	rsp.IdCard = user.Id_card
+	rsp.AvatarUrl = user.Avatar_url
+
+	return nil
 }
